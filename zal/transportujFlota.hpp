dc08c9b050f279d870c70703392e73f22ce3b75d
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2020-11-25 20:15:37 +0000

    xd

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,17 +6,19 @@ using namespace std;
 unsigned int transportujFlota(unsigned int towar)
 {
   int PrzewiezionyTowar = 0;
-  int suma=0;
+  
   int licz =0;
   if(towar == 0){
     return 0;
   }
   else{
-    while(suma<towar){
+    int suma=0;
+    while(suma<towar)
+    {
     Stocznia stocznia{};
     Statek* galeon = stocznia();
     PrzewiezionyTowar = galeon->transportuj();
-    suma = suma + PrzewiezionyTowar;
+    suma += PrzewiezionyTowar;
     if (galeon == dynamic_cast<Zaglowiec*>(galeon))
     {
      licz++;
@@ -24,6 +26,7 @@ unsigned int transportujFlota(unsigned int towar)
 
     }
     delete galeon;
+    return licz;
   }
-  return licz;
+  
 }
