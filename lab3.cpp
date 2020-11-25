e7c7e34df75d8422ced82e1b65b8148e685d1a7b
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2020-11-25 20:08:11 +0000

    coś tam coś

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
+using namespace std;
+
+class BytGeometryczny
+{
+    virtual void id() = 0;
+};
+class Figura : public BytGeometryczny
+{
+public:
+    Figura() : pole{12} {};
+    Figura(double x) : pole(x){};
+    // double getter() { return pole; };
+    // double setter(double x) { pole = x; };
+    virtual void id()
+    {
+        cout << "Typ figury: "
+             << "pole: " << pole << endl;
+    }
+    virtual ~Figura() { cout << "zniszczylem figure" << endl; }
+
+protected:
+    double pole;
+};
+
+class Kolo : public Figura
+{
+public:
+    Kolo() {}
+    Kolo(double R) : Figura{3.14 * R * R} {};
+    void id() override
+    {
+        cout << "Typ figury: Kolo "
+             << "pole: " << pole << endl;
+    }
+    virtual ~Kolo() { cout << "Zniszczylem kolo " << endl; }
+};
+
+class Kwadrat : public Figura
+{
+public:
+    Kwadrat() {}
+    Kwadrat(double a) : Figura{a * a} {}
+    void id() override
+    {
+        cout << "Typ figury: Kwadrat "
+             << "pole: " << pole << endl;
+    }
+    virtual ~Kwadrat() { cout << "Zniszczylem kwadrat " << endl; }
+};
+void id(Figura& fig)
+{
+    fig.id();
+}
+class WektorFigur
+{
+public:
+    WektorFigur() { tab = new Figura[1000]; }
+
+    void push(const Figura& fig)
+    {
+        tab[licznik] = fig;
+        licznik++;
+    }
+    Figura* operator[](int index)
+    {
+        if (index < licznik) {
+            return &tab[index];
+        }
+        else {
+            return nullptr;
+        }
+    };
+
+    int     licznik = 0;
+    Figura* tab;
+};
 
 int main()
-{
-    puts("Hello, World!");
-}
+{}
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,9 +1,29 @@
 #pragma once
 
 #include "Stocznia.hpp"
+using namespace std;
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+  int PrzewiezionyTowar = 0;
+  int suma=0;
+  int licz =0;
+  if(towar == 0){
     return 0;
+  }
+  else{
+    while(suma<towar){
+    Stocznia stocznia{};
+    Statek* galeon = stocznia();
+    PrzewiezionyTowar = galeon->transportuj();
+    suma = suma + PrzewiezionyTowar;
+    if (galeon == dynamic_cast<Zaglowiec*>(galeon))
+    {
+     licz++;
+    }
+
+    }
+    delete galeon;
+  }
+  return licz;
 }
